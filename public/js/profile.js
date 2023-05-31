5b682b3a88699ba7a97e6fd144b81014d466a19b
jdel-18 <joshuadelves1@yahoo.com>
2023-05-30 20:02:49 -0500

    update

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,7 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const name = document.querySelector('#post-name').value.trim();
-    const [post] = document.querySelector('#post').value.trim();
+    const post = document.querySelector('#post').value.trim();
     const description = document.querySelector('#post-desc').value.trim();
   
     if (name && post && description) {
